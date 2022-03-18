9a57fb20e6fe4bb34eee3ca8e935fb339e648f0c
Pratik Singh <singhpratik8828@gmail.com>
2022-03-18 23:48:35 +0530

    Initial commit

---
+++ ferb.js
@@ -0,0 +1,27 @@
+var button = document.querySelector("#btn");
+
+var input = document.querySelector("#text-input");
+
+var output = document.querySelector("#text-output");
+
+var url = "https://api.funtranslations.com/translate/ferb-latin.json";
+
+function getTranslation(text) {
+  return url + "?" + "text=" + text;
+}
+
+function errorHandler(error) {
+  console.log("error occured", error);
+  alert("something went wrong! Please try again.");
+}
+
+function getClicked() {
+  fetch(getTranslation(input.value))
+    .then((response) => response.json())
+    .then((json) => {
+      var translated = json.contents.translated;
+      output.innerText = translated;
+    })
+    .catch(errorHandler);
+}
+button.addEventListener("click", getClicked);
